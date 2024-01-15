47b4a27ffe5c059f4a254541b9de0f4bc08a0032
Valentyn Solomko <vsolomko@cloudbees.com>
2024-01-15 16:07:46 +0200

    Debugging 2

--- specs/src/test/groovy/com/cloudbees/plugin/spec/RunCustomCommandTest.groovy
+++ specs/src/test/groovy/com/cloudbees/plugin/spec/RunCustomCommandTest.groovy
@@ -61,106 +61,103 @@ class RunCustomCommandTest extends PluginTestHelper {
         println(response.jobLink)
         assert response.isSuccessful()
         assert response.outcome == outcome
-        String sList = response.jobProperties["runCustomCommand"].toString()
-        println("#001: "+sList.length())
-        println("#002: "+sList)
-        List<Map<String, String>> list = (new JsonSlurper()).parseText(sList) as List<Map>
-        println("#003: "+list.size())
-//        Map<String, String> found = findRecord(list, name, what)
-//        assert found
+        List<Map<String, String>> list = (new JsonSlurper()).parseText(response.jobProperties["runCustomCommand"].toString()) as List<Map>
+        Map<String, String> found = findRecord(list, name, what)
+        assert found
 
         where:
         caseId | group     | command         | subCommands | options         | outcome            | name   | what
-//        "#010" | "compute" | "zones"         | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "us-east1-b"
-//        "#011" | "compute" | "disk-types"    | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "pd-standard"
-        "#012" | "compute" | "machine-types" | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "n1-standard-1"
+        "#010" | "compute" | "zones"         | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "us-east1-b"
+        "#011" | "compute" | "disk-types"    | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "pd-standard"
+// hangs on JsonSlurper
+//        "#012" | "compute" | "machine-types" | "list"      | "--format=json" | JobOutcome.SUCCESS | "name" | "n1-standard-1"
     }
-//
-//    @Unroll
-//    def 'RunCustomCommand complex'() {
-//        when:
-//        GCloudConfig gcloudConfig = GCloudConfig.create(plugin)
-//        gcloudConfig.gcloudPath(gcloudPath)
-//                .authType('key')
-//                .credential('', gcloudKey)
-//                .gcloudConfigurationName(gcloudConfigurationName)
-//                .projectName(gcloudProject)
-//                .gcloudProprties("""compute/zone ${gcloudZone}""")
-//                .checkConnectionResource(resource.getName())
-//                .debugLevel("2")
-//
-//        setConfigurationValues(Utils.CredsStates.RUNTIME, credentialFieldName, credential, gcloudConfig)
-//        plugin.configure(gcloudConfig)
-//
-//        and: "Run Plugin Procedure - Run Custom Command"
-//        JobResponse response = plugin.runCustomCommand
-//                .group('compute')
-//                .command('instances')
-//                .subCommands('list')
-//                .options("--format='json(name,zone)'")
-//                .resultPropertySheet("/myJob/runCustomCommand")
-//                .run(runOpts)
-//
-////        Getting name and zone of first instance from output
-//        List<Map<String, String>> instances = (new JsonSlurper()).parseText(response.jobProperties["runCustomCommand"].toString()) as List<Map>
-//
-////        Running "compute instances describe <instance> --zone=<zone> --format=json"
-//        response = plugin.runCustomCommand
-//                .group('compute')
-//                .command('instances')
-//                .subCommands("""describe
-//${instances[0].name}""")
-//                .options("""--zone=${instances[0].zone}
-//--format=json""")
-//                .resultPropertySheet("/myJob/runCustomCommand")
-//                .run(runOpts)
-//
-//        then:
-//
-//        println(response.jobLink)
-//        assert response.isSuccessful()
-//        assert response.outcome == JobOutcome.SUCCESS
-//    }
-//
-//    @Unroll
-//    def 'RunCustomCommand negative: #caseId'() {
-//        when:
-//        GCloudConfig gcloudConfig = GCloudConfig.create(plugin)
-//        gcloudConfig.gcloudPath(gcloudPath)
-//                .authType('key')
-//                .credential('', gcloudKey)
-//                .gcloudConfigurationName(gcloudConfigurationName)
-//                .projectName(gcloudProject)
-//                .gcloudProprties("""compute/zone ${gcloudZone}""")
-//                .checkConnectionResource(resource.getName())
-//                .debugLevel("2")
-//
-//        setConfigurationValues(Utils.CredsStates.RUNTIME, credentialFieldName, credential, gcloudConfig)
-//        plugin.configure(gcloudConfig)
-//
-//        and: "Run Plugin Procedure - Run Custom Command"
-//        JobResponse response = plugin.runCustomCommand
-//                .group(group)
-//                .command(command)
-//                .subCommands(subCommands)
-//                .options(options)
-//                .resultPropertySheet("/myJob/runCustomCommand")
-//                .run(runOpts)
-//
-//        then:
-//        println(response.jobLink)
-//        assert !response.isSuccessful()
-//        assert response.outcome == JobOutcome.ERROR
-//
-//        assert response.jobSteps.last().summary =~ /${message}/
-//
-//        where:
-//        caseId                  | group     | command         | subCommands | options         | message
-//        "invalid group"         | "invalid" | ""            | ""            | ""        | "Invalid choice"
-//        "invalid command"       | "compute" | "invalid"     | ""            | ""        | "Invalid choice"
-//        "invalid sub-command"   | "compute" | "zones"       | "invalid"     | ""        | "Invalid choice"
-//        "invalid zone"          | "compute" | "zones"       | "describe"    | "invalid" | "The resource 'projects/${gcloudProject}/zones/invalid' was not found"
-//        "invalid instance"      | "compute" | "instances"   | "describe"    | "invalid" | "The resource 'projects/${gcloudProject}/zones/${gcloudZone}/instances/invalid' was not found"
-//    }
+
+    @Unroll
+    def 'RunCustomCommand complex'() {
+        when:
+        GCloudConfig gcloudConfig = GCloudConfig.create(plugin)
+        gcloudConfig.gcloudPath(gcloudPath)
+                .authType('key')
+                .credential('', gcloudKey)
+                .gcloudConfigurationName(gcloudConfigurationName)
+                .projectName(gcloudProject)
+                .gcloudProprties("""compute/zone ${gcloudZone}""")
+                .checkConnectionResource(resource.getName())
+                .debugLevel("2")
+
+        setConfigurationValues(Utils.CredsStates.RUNTIME, credentialFieldName, credential, gcloudConfig)
+        plugin.configure(gcloudConfig)
+
+        and: "Run Plugin Procedure - Run Custom Command"
+        JobResponse response = plugin.runCustomCommand
+                .group('compute')
+                .command('instances')
+                .subCommands('list')
+                .options("--format='json(name,zone)'")
+                .resultPropertySheet("/myJob/runCustomCommand")
+                .run(runOpts)
+
+//        Getting name and zone of first instance from output
+        List<Map<String, String>> instances = (new JsonSlurper()).parseText(response.jobProperties["runCustomCommand"].toString()) as List<Map>
+
+//        Running "compute instances describe <instance> --zone=<zone> --format=json"
+        response = plugin.runCustomCommand
+                .group('compute')
+                .command('instances')
+                .subCommands("""describe
+${instances[0].name}""")
+                .options("""--zone=${instances[0].zone}
+--format=json""")
+                .resultPropertySheet("/myJob/runCustomCommand")
+                .run(runOpts)
+
+        then:
+
+        println(response.jobLink)
+        assert response.isSuccessful()
+        assert response.outcome == JobOutcome.SUCCESS
+    }
+
+    @Unroll
+    def 'RunCustomCommand negative: #caseId'() {
+        when:
+        GCloudConfig gcloudConfig = GCloudConfig.create(plugin)
+        gcloudConfig.gcloudPath(gcloudPath)
+                .authType('key')
+                .credential('', gcloudKey)
+                .gcloudConfigurationName(gcloudConfigurationName)
+                .projectName(gcloudProject)
+                .gcloudProprties("""compute/zone ${gcloudZone}""")
+                .checkConnectionResource(resource.getName())
+                .debugLevel("2")
+
+        setConfigurationValues(Utils.CredsStates.RUNTIME, credentialFieldName, credential, gcloudConfig)
+        plugin.configure(gcloudConfig)
+
+        and: "Run Plugin Procedure - Run Custom Command"
+        JobResponse response = plugin.runCustomCommand
+                .group(group)
+                .command(command)
+                .subCommands(subCommands)
+                .options(options)
+                .resultPropertySheet("/myJob/runCustomCommand")
+                .run(runOpts)
+
+        then:
+        println(response.jobLink)
+        assert !response.isSuccessful()
+        assert response.outcome == JobOutcome.ERROR
+
+        assert response.jobSteps.last().summary =~ /${message}/
+
+        where:
+        caseId                  | group     | command         | subCommands | options         | message
+        "invalid group"         | "invalid" | ""            | ""            | ""        | "Invalid choice"
+        "invalid command"       | "compute" | "invalid"     | ""            | ""        | "Invalid choice"
+        "invalid sub-command"   | "compute" | "zones"       | "invalid"     | ""        | "Invalid choice"
+        "invalid zone"          | "compute" | "zones"       | "describe"    | "invalid" | "The resource 'projects/${gcloudProject}/zones/invalid' was not found"
+        "invalid instance"      | "compute" | "instances"   | "describe"    | "invalid" | "The resource 'projects/${gcloudProject}/zones/${gcloudZone}/instances/invalid' was not found"
+    }
 
 }
